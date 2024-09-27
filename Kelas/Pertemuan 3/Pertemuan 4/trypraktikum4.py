42b901fc13829c3cdc8ffb5af4af86e58bb40b3f
ridwantzy <r1dw4n230806@gmail.com>
2024-09-27 15:09:18 +0800

    Add files via upload

---
+++ Kelas/Pertemuan 3/Pertemuan 4/trypraktikum4.py
@@ -0,0 +1,101 @@
+# ulang = 10
+# for i in range(ulang):
+    # print("perulangan ke-" + str(i))
+    # print(f"Perulangan ke--{i} kali")
+
+# for i in range(1, 11, 2):
+#     print(f"Perulangan ke--{i} kali")
+
+# simpan =  [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+# # for i in range(len(simpan))
+#     print(i)
+
+# print(simpan[2])
+
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+
+# print(“Daftar  bilangan ganjil dari 1-10”)
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+
+# for i in range(1, 6, 2):
+#     for j in range(1, 6, 2):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+
+# for i in range(10,-1, -1):
+#     print(f"Anak ayam turun {i}")
+
+
+# # ulang = "ya"
+# hitung = 1
+# while true :
+#     print(f"perulangan ke {hitung +1}")
+#     # ulang = input("Apakah anda masih ingin mnegulang?")
+#     hitung +=1
+# print("perulangan selesai")
+
+# # ulang == "ya
+
+# x = 0
+# while x < 5:
+#     print(x)
+#     x+=1
+
+# hitung = 1
+# while true:
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("Apakah ingin mengulang lagi?")
+#     if ulang == "tidak":
+#         print("perulangan berhenti")
+#         break
+#     hitung +=1
+
+# print(f"Total perulangan: {hitung}")
+
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         break # langsung keluar dari perulangan (iterasi)
+#     if i == 5:
+#         continue  # lompat ke literasi selanjutnya
+#     print(i)
+
+
+# usn = "admin"
+# pw = "admin123"
+# salah = 0
+# while salah < 3:
+#     username = input("Masukkan username: ")
+#     password = input("Masukkan password: ")
+#     if usn == username and pw == password:
+#         print("LOgin berhasil")
+#         break
+#     else:
+#         print("login gagal")
+#         salah +=1
+
+
+
+
+
