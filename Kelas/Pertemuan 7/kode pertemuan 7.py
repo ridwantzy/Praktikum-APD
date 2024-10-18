59e3a5f64010e48b9b508a9bd1442ef78e303505
ridwantzy <r1dw4n230806@gmail.com>
2024-10-18 15:27:59 +0800

    Add files via upload

---
+++ Kelas/Pertemuan 7/kode pertemuan 7.py
@@ -0,0 +1,87 @@
+# def tambah():
+#     print ("ini adalah fungsi di Python")
+
+# def tambah(a,b):
+#     hasil = a+b
+#     print(hasil)
+
+# tambah(10,2)
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# tambahan = tambah(2,4) + tambah(4,6)
+
+# print(tambahan)
+  
+# def pilihan(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print(2)
+#             return
+#     print(3)
+
+# pilihan(2)
+            
+# def cetak(nim, nama,  **alamat):
+#     print(nama,  nim, alamat)
+
+# cetak("rdwn", 41, biolohi= 23, hahah=43)
+
+# var=2,3,4
+# print(var)  # Output: (2, 3, 4)  # Tuple
+# var1, var2, var3 = var
+# print(var)
+# print(var1)  # Output: 2
+# print(var3)
+
+# membuat variabel global
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+    
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+
+# info()
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+
+
+# def cetak(nama,nim, kelas=23):
+#     print(nama, nim, kelas)
+
+# if __name__ == "__main__":
+#     while(True):
+#         def cetak(nama,nim, kelas=23)
+# # kodingan utama dan gbsa dimpott
+
+# def operasi_aritmatika(a, b):
+#     infopenjumlahan = a + b
+#     pengurangan = a - b
+#     perkalian = a * b
+#     pembagian = a / b
+#     # return penjumlahan, pengurangan, perkalian, pembagian
+
+# hasil = operasi_aritmatika(10, 5)
+# for i in hasil:
+#     print(i)  # Output: (15, 5, 50, 2.0)
+
+# buku =["kimia", "bahsa"]
+# buku_baru = input("Judul Buku : ")
+# buku.append(buku_baru)
+# def show_data():
+#     if len(buku) <= 0:
+#         print ("Belum Ada data")
+#     else:
+#         print("ID", "Nama Buku")
+#         for indeks in range(len(buku)) :
+#             print(indeks +1, buku[indeks])
+# show_data()
